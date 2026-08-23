29b0806b01a70050917b4be48fde09c4a17551e1
myanaesthesiology <anaesthesiology01@gmail.com>
2026-08-24 00:26:31 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.ICU_CONFIG = Object.freeze({
+  // Paste the deployed Google Apps Script web-app URL ending in /exec.
+  // Example: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
+  GAS_WEB_APP_URL: 'PASTE-YOUR-APPS-SCRIPT-EXEC-URL-HERE'
+});
