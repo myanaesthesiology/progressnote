778684bf38d19a1d1c0681fb652d8f7758ed43bb
myanaesthesiology <anaesthesiology01@gmail.com>
2026-08-24 00:36:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.ICU_CONFIG = Object.freeze({
   // Paste the deployed Google Apps Script web-app URL ending in /exec.
-  // Example: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
+  // Example: https://script.google.com/macros/s/AKfycby4ZH-9HoJ2K9rz6RW49jOsLDddM6Bek7mhQsOAIvlDLOQGI1RcC9007zUlmjIIREPmvg/exec
   GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycby4ZH-9HoJ2K9rz6RW49jOsLDddM6Bek7mhQsOAIvlDLOQGI1RcC9007zUlmjIIREPmvg/exec'
 });
